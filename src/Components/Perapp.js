d9a57874a317d1eb8bc105cbbdd26e4184a95d33
Srikari Amrutham <amruthamsrikari@gmail.com>
2023-02-04 22:13:31 +0530

    commit

--- src/Components/Perapp.js
+++ src/Components/Perapp.js
@@ -1,120 +1,109 @@
 import React from "react";
 export default function Licapp(){
     return <>
-        <div className="container-fluid">
-
+    <div className="container-fluid">
         <h2>Permit Application forms</h2>
         <br/>
         <div className="table-responsive">
-
-        <table className="table container" id="Info">
-            <tr>
-                <th>Applicant Name</th>
-                <th>Application form</th>
+            <table className="table container" id="Info">
+                <tr>
+                    <th>Applicant Name</th>
+                    <th>Application form</th>
+                </tr>
+                <tr>
+                    <td>Name1</td>
+                    <td><button style={{color:"black",backgroundColor:"white"}}type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#myModal">view details</button></td>
+                </tr>
+                <tr>
+                    <td>Name1</td>
+                    <td><button style={{color:"black",backgroundColor:"white"}} type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#myModal">
+                        view details
+                    </button></td>
+                    
+                </tr>
+                <tr>
+                    <td>Name1</td>
+                    <td><button style={{color:"black",backgroundColor:"white"}}type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#myModal">
+                        view details
+                    </button></td>
+                    
+                </tr>
+                <tr>
+                    <td>Name1</td>
+                    <td><button style={{color:"black",backgroundColor:"white"}}type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#myModal">
+                        view details
+                    </button></td>
+                    
+                </tr>
                 
-            </tr>
-            <tr>
-                <td>Name1</td>
-                <td><button style={{color:"black",backgroundColor:"white"}}type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#myModal">
-                    view details
-                </button></td>
-                
-            </tr>
-            <tr>
-                <td>Name1</td>
-                <td><button style={{color:"black",backgroundColor:"white"}} type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#myModal">
-                    view details
-                </button></td>
-                
-            </tr>
-            <tr>
-                <td>Name1</td>
-                <td><button style={{color:"black",backgroundColor:"white"}}type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#myModal">
-                    view details
-                </button></td>
-                
-            </tr>
-            <tr>
-                <td>Name1</td>
-                <td><button style={{color:"black",backgroundColor:"white"}}type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#myModal">
-                    view details
-                </button></td>
-                
-            </tr>
-            
-        </table>  
+            </table>  
         </div>
         </div>
         <div className="modal" id="myModal">
-        <div className="modal-dialog">
-        <div className="modal-content">
-
-            <div className="modal-header">
-            <h4 className="modal-title">Form</h4>
-            <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
-            </div>
-
-            <div className="modal-body">
-                <table className="table-bordered" id="Info">
-                    <table className=" table table-striped table-light">
-                    <tr>
-                        <th>Name:</th><td></td>
-                    </tr>
-                    <tr>
-                        <th>Age:</th><td></td>
-                    </tr>
-                    <tr>
-                        <th>Gender:</th><td></td>
-                    </tr>
-                    <tr>
-                        <th>Date of birth:</th><td></td>
-                    </tr>
-                    <tr>
-                        <th>Temp Address:</th><td></td>
-                    </tr>
-                    <tr>
-                        <th>Prem Address:</th><td></td>
-                    </tr>
-                    <tr>
-                        <th>Aadhar number:</th><td></td>
-                    </tr>
-                    <tr>
-                        <th>Phone number:</th><td></td>
-                    </tr>
-                    <tr>
-                        <th>Email:</th><td></td>
-                    </tr>
-                    <tr>
-                        <th>License number:</th><td></td>
-                    </tr>
-                    <tr>
-                        <th>RC number:</th><td></td>
-                    </tr>
-                    <tr>
-                        <th>Departure Date:</th><td></td>
-                    </tr>
-                    <tr>
-                        <th>Reason:</th><td></td>
-                    </tr>
-                    <tr>
-                        <th>Desired permit:</th><td></td>
-                    </tr> 
-                    </table>
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h4 className="modal-title">Form</h4>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
+                    </div>
+                    <div className="modal-body">
+                    <table className="table-bordered" id="Info">
+                        <table className=" table table-striped table-light">
+                            <tr>
+                                <th>Name:</th><td></td>
+                            </tr>
+                            <tr>
+                                <th>Age:</th><td></td>
+                            </tr>
+                            <tr>
+                                <th>Gender:</th><td></td>
+                            </tr>
+                            <tr>
+                                <th>Date of birth:</th><td></td>
+                            </tr>
+                            <tr>
+                                <th>Temp Address:</th><td></td>
+                            </tr>
+                            <tr>
+                                <th>Prem Address:</th><td></td>
+                            </tr>
+                            <tr>
+                                <th>Aadhar number:</th><td></td>
+                            </tr>
+                            <tr>
+                                <th>Phone number:</th><td></td>
+                            </tr>
+                            <tr>
+                                <th>Email:</th><td></td>
+                            </tr>
+                            <tr>
+                                <th>License number:</th><td></td>
+                            </tr>
+                            <tr>
+                                <th>RC number:</th><td></td>
+                            </tr>
+                            <tr>
+                                <th>Departure Date:</th><td></td>
+                            </tr>
+                            <tr>
+                                <th>Reason:</th><td></td>
+                            </tr>
+                            <tr>
+                                <th>Desired permit:</th><td></td>
+                            </tr> 
+                        </table>
                     </table>
                     </div>
-            <br/>
-            
-            <div style={{width: "auto",justifyContent: "center",display: "flex"}} className="row">
-            <input style={{width: "100px",marginRight: "5px"}} type="approve" className="btn btn-success" value="Approve"/>
-            <input style={{width: "100px"}} type="reject" className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#myModal" value="Reject"/>
+                    <br/>
+                <div style={{width: "auto",justifyContent: "center",display: "flex"}} className="row">
+                <input style={{width: "100px",marginRight: "5px"}} type="approve" className="btn btn-success" value="Approve"/>
+                <input style={{width: "100px"}} type="reject" className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#myModal" value="Reject"/>
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-danger " data-bs-dismiss="modal">Close</button>
+                </div>
+                </div>
             </div>
-
-            <div className="modal-footer">
-            <button type="button" className="btn btn-danger " data-bs-dismiss="modal">Close</button>
-            </div>
-
         </div>
-        </div>
-    </div>
     </>
 }
